4fc7e960ad698c52d16faba36efac6e60fe1174e
PUGE <mail@puge.me>
2019-01-07 17:03:28 +0800

    摄影作品

---
+++ image/core/main.js
@@ -0,0 +1,96 @@
+// 获取URL #后面内容
+function getarg(url){
+  arg = url.split("#");
+  return arg[1];
+}
+
+// 页面资源加载完毕事件
+window.onload = function() {
+  // 取出URL地址判断当前所在页面
+  var pageArg = getarg(window.location.href)
+  // 从配置项中取出程序入口
+  var page = pageArg ? pageArg : globalConfig.entry
+  if (page) {
+    var entryDom = document.getElementById('page-id-' + page)
+    if (entryDom) {
+      // 显示主页面
+      entryDom.style.display = 'block'
+      runPageFunction(page, entryDom)
+    } else {
+      console.error('入口文件设置错误!')
+    }
+  } else {
+    console.error('未设置程序入口!')
+  }
+}
+
+// PG-name处理
+function pgNameHandler (dom) {
+  // 遍历每一个DOM节点
+  for (var i = 0; i < dom.children.length; i++) {
+    var tempDom = dom.children[i]
+    // 判断是否存在pg-name属性
+    const pgName = tempDom.attributes['pg-name']
+    if (pgName) {
+      window.PG.domList[pgName.textContent] = tempDom
+    }
+    // 递归处理所有子Dom结点
+    if (tempDom.children.length > 0) {
+      pgNameHandler(tempDom)
+    }
+  }
+}
+
+// 运行页面所属的方法
+function runPageFunction (pageName, entryDom) {
+  // PG-name处理
+  window.PG.domList = {}
+  pgNameHandler(entryDom)
+
+  // 判断页面是否有自己的方法
+  var newPageFunction = window.PG.script[pageName]
+  // 如果有方法,则运行它
+  if (newPageFunction) {
+    newPageFunction.created.apply(window.PG)
+  }
+}
+
+
+// url发生改变事件
+window.onhashchange = function(e) {
+  var oldUrlParam = getarg(e.oldURL)
+  var newUrlParam = getarg(e.newURL)
+  // 查找页面跳转前的page页(dom节点)
+  // console.log(oldUrlParam)
+  // 如果源地址获取不到 那么一般是因为源页面为首页
+  if (oldUrlParam === undefined) {
+    oldUrlParam = globalConfig.entry
+  }
+  var oldDom = document.getElementById('page-id-' + oldUrlParam)
+  if (oldDom) {
+    // 隐藏掉旧的节点
+    oldDom.style.display = 'none'
+  }
+  // 查找页面跳转后的page
+  
+  var newDom = document.getElementById('page-id-' + newUrlParam)
+  // console.log(newDom)
+  if (newDom) {
+    // 隐藏掉旧的节点
+    newDom.style.display = 'block'
+  } else {
+    console.error('页面不存在!')
+  }
+  runPageFunction(newUrlParam, entryDom)
+}
+
+// dom点击事件处理
+function pgClick (item) {
+  // 判断页面是否有自己的方法
+  var newPageFunction = window.PG.script[item.name]
+  // 如果有方法,则运行它
+  if (newPageFunction && newPageFunction.methods[item.methodName]) {
+    // 绑定window.PG对象
+    newPageFunction.methods[item.methodName].apply(window.PG, [item])
+  }
+}
---
+++ image/lib/style.js
@@ -0,0 +1,20 @@
+'use strict'
+const Templet = require('./templet')
+
+function styleHandle (htmlText, name) {
+  // console.log(htmlText)
+  const styleText = Templet.cutString(htmlText, "<style>", "</style>")
+  // 取出所有Body标识
+  const styleArr = Templet.cutStringArray(styleText, "{", "}")
+  // 取出所有的标签名
+  const styleNameArr = Templet.cutStringArray(styleText, `\n`, "{", 0 , true)
+  // console.log(styleArr)
+  let newStyleNameArr = []
+  // console.log(styleNameArr)
+  for (let key in styleNameArr) {
+    newStyleNameArr[key] = `.page-class-${name} ${Templet.trim(styleNameArr[key])} {${styleArr[key]}}`
+  }
+  // console.log(newStyleNameArr)
+  return newStyleNameArr
+}
+module.exports = styleHandle
