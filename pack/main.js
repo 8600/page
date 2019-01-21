126b5d774db6e8b62b6eb0cc719e327e141cb732
PUGE <mail@puge.me>
2019-01-21 15:05:56 +0800

    去掉老旧的项目

--- pack/main.js
+++
@@ -1,8 +0,0 @@
-const Templet = require('./lib/templet')
-const fs = require('fs')
-
-
-// 读取模板文件
-// 将整个文件一次性读取到内存中
-const templet = fs.readFileSync('./index.html', 'utf8');
-console.log(Templet.cutString("sdsdsddddddddd", "s", "ddddd"))
