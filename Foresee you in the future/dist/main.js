2264435d63bb6278d910e9a40c57e06c4186cc60
PUGE <mail@puge.me>
2019-02-12 10:11:18 +0800

    使用gif

--- Foresee you in the future/dist/main.js
+++ Foresee you in the future/dist/main.js
@@ -164,8 +164,9 @@ window.ozzx.script = {
       "people": null,
       "peopleImgID": 1,
       "xMax": 0,
+      "resources": null,
       "peopleIsMoveing": false,
-      "imgArr": ["./images/1.png", "./images/3-colour.png", "./images/1-clock.png", "./images/1-point.png", "./images/1-hand.png", "./images/1-butterfly.png", "./images/people-1.png", "./images/people-2.png", "./images/people-3.png", "./images/people-4.png", "./images/people-5.png", "./images/people-6.png", "./images/people-7.png", "./images/people-8.png", "./images/people-9.png", "./images/people-10.png", "./images/people-11.png", "./images/2.png", "./images/3.png", "./images/3-thought.png", "./images/3-start.png", "./images/3-light.png", "./images/4.png", "./images/5.png", "./images/6.png", "./images/7.png", "./images/8.png", "./images/9.png", "./images/10.png", "./images/11.png", "./images/share.png", "./images/5-colour.png", "./images/7-light.png", "./images/7-colour.png", "./images/9-colour.png", "./images/11-light.png", "./images/11-colour.png", "./images/3-cloud.png", "./images/4-cloud.png", "./images/5-cloud.png", "./images/6-cloud.png", "./images/7-cloud.png", "./images/8-cloud.png", "./images/9-cloud.png", "./images/10-cloud.png", "./images/11-petal-colour.png", "./images/1/1.png", "./images/1/2.png", "./images/1/3.png", "./images/1/4.png", "./images/1/5.png", "./images/1/6.png", "./images/1/7.png", "./images/1/8.png", "./images/1/9.png", "./images/1/10.png", "./images/1/11.png", "./images/1/12.png", "./images/1/13.png", "./images/1/14.png", "./images/1/15.png", "./images/1/16.png", "./images/2/1.png", "./images/2/2.png", "./images/2/3.png", "./images/2/4.png", "./images/2/5.png", "./images/2/6.png", "./images/2/7.png", "./images/2/8.png", "./images/2/9.png", "./images/2/10.png", "./images/2/11.png", "./images/2/12.png", "./images/2/13.png", "./images/2/14.png", "./images/2/15.png", "./images/2/16.png", "./images/3/1.png", "./images/3/2.png", "./images/3/3.png", "./images/3/4.png", "./images/3/5.png", "./images/3/6.png", "./images/3/7.png", "./images/3/8.png", "./images/3/9.png", "./images/3/10.png", "./images/3/11.png", "./images/3/12.png", "./images/3/13.png", "./images/3/14.png", "./images/3/15.png", "./images/3/16.png", "./images/4/1.png", "./images/4/2.png", "./images/4/3.png", "./images/4/4.png", "./images/4/5.png", "./images/4/6.png", "./images/4/7.png", "./images/4/8.png", "./images/4/9.png", "./images/4/10.png", "./images/4/11.png", "./images/4/12.png", "./images/4/13.png", "./images/4/14.png", "./images/4/15.png", "./images/4/16.png", "./images/5/1.png", "./images/5/2.png", "./images/5/3.png", "./images/5/4.png", "./images/5/5.png", "./images/5/6.png", "./images/5/7.png", "./images/5/8.png", "./images/5/9.png", "./images/5/10.png", "./images/5/11.png", "./images/5/12.png", "./images/5/13.png", "./images/5/14.png", "./images/5/15.png", "./images/5/16.png", "./images/6/1.png", "./images/6/2.png", "./images/6/3.png", "./images/6/4.png", "./images/6/5.png", "./images/6/6.png", "./images/6/7.png", "./images/6/8.png", "./images/6/9.png", "./images/6/10.png", "./images/6/11.png", "./images/6/12.png", "./images/6/13.png", "./images/6/14.png", "./images/6/15.png", "./images/6/16.png", "./images/7/1.png", "./images/7/2.png", "./images/7/3.png", "./images/7/4.png", "./images/7/5.png", "./images/7/6.png", "./images/7/7.png", "./images/7/8.png", "./images/7/9.png", "./images/7/10.png", "./images/7/11.png", "./images/7/12.png", "./images/7/13.png", "./images/7/14.png", "./images/7/15.png", "./images/7/16.png", "./images/8/1.png", "./images/8/2.png", "./images/8/3.png", "./images/8/4.png", "./images/8/5.png", "./images/8/6.png", "./images/8/7.png", "./images/8/8.png", "./images/8/9.png", "./images/8/10.png", "./images/8/11.png", "./images/8/12.png", "./images/8/13.png", "./images/8/14.png", "./images/8/15.png", "./images/8/16.png", "./images/9/1.png", "./images/9/2.png", "./images/9/3.png", "./images/9/4.png", "./images/9/5.png", "./images/9/6.png", "./images/9/7.png", "./images/9/8.png", "./images/9/9.png", "./images/9/10.png", "./images/9/11.png", "./images/9/12.png", "./images/9/13.png", "./images/9/14.png", "./images/9/15.png", "./images/9/16.png", "./images/grass1.png", "./images/sport/1.png", "./images/sport/2.png", "./images/support.png", "./images/showMore.png", "./images/support-tree-1.png"],
+      "imgArr": ["./images/1.png", "./images/3-colour.png", "./images/1-clock.png", "./images/1-point.png", "./images/1-hand.png", "./images/1-butterfly.png", "./images/people-1.png", "./images/people-2.png", "./images/people-3.png", "./images/people-4.png", "./images/people-5.png", "./images/people-6.png", "./images/people-7.png", "./images/people-8.png", "./images/people-9.png", "./images/people-10.png", "./images/people-11.png", "./images/2.png", "./images/3.png", "./images/3-thought.png", "./images/3-start.png", "./images/3-light.png", "./images/4.png", "./images/5.png", "./images/6.png", "./images/7.png", "./images/8.png", "./images/9.png", "./images/10.png", "./images/11.png", "./images/share.png", "./images/5-colour.png", "./images/7-light.png", "./images/7-colour.png", "./images/9-colour.png", "./images/11-light.png", "./images/11-colour.png", "./images/3-cloud.png", "./images/4-cloud.png", "./images/5-cloud.png", "./images/6-cloud.png", "./images/7-cloud.png", "./images/8-cloud.png", "./images/9-cloud.png", "./images/10-cloud.png", "./images/11-petal-colour.png", "./images/1/1.png", "./images/1/2.png", "./images/1/3.png", "./images/1/4.png", "./images/1/5.png", "./images/1/6.png", "./images/1/7.png", "./images/1/8.png", "./images/1/9.png", "./images/1/10.png", "./images/1/11.png", "./images/1/12.png", "./images/1/13.png", "./images/1/14.png", "./images/1/15.png", "./images/1/16.png", "./images/2/1.png", "./images/2/2.png", "./images/2/3.png", "./images/2/4.png", "./images/2/5.png", "./images/2/6.png", "./images/2/7.png", "./images/2/8.png", "./images/2/9.png", "./images/2/10.png", "./images/2/11.png", "./images/2/12.png", "./images/2/13.png", "./images/2/14.png", "./images/2/15.png", "./images/2/16.png", "./images/3/1.png", "./images/3/2.png", "./images/3/3.png", "./images/3/4.png", "./images/3/5.png", "./images/3/6.png", "./images/3/7.png", "./images/3/8.png", "./images/3/9.png", "./images/3/10.png", "./images/3/11.png", "./images/3/12.png", "./images/3/13.png", "./images/3/14.png", "./images/3/15.png", "./images/3/16.png", "./images/4/1.png", "./images/4/2.png", "./images/4/3.png", "./images/4/4.png", "./images/4/5.png", "./images/4/6.png", "./images/4/7.png", "./images/4/8.png", "./images/4/9.png", "./images/4/10.png", "./images/4/11.png", "./images/4/12.png", "./images/4/13.png", "./images/4/14.png", "./images/4/15.png", "./images/4/16.png", "./images/5/1.png", "./images/5/2.png", "./images/5/3.png", "./images/5/4.png", "./images/5/5.png", "./images/5/6.png", "./images/5/7.png", "./images/5/8.png", "./images/5/9.png", "./images/5/10.png", "./images/5/11.png", "./images/5/12.png", "./images/5/13.png", "./images/5/14.png", "./images/5/15.png", "./images/5/16.png", "./images/6/1.png", "./images/6/2.png", "./images/6/3.png", "./images/6/4.png", "./images/6/5.png", "./images/6/6.png", "./images/6/7.png", "./images/6/8.png", "./images/6/9.png", "./images/6/10.png", "./images/6/11.png", "./images/6/12.png", "./images/6/13.png", "./images/6/14.png", "./images/6/15.png", "./images/6/16.png", "./images/7/1.png", "./images/7/2.png", "./images/7/3.png", "./images/7/4.png", "./images/7/5.png", "./images/7/6.png", "./images/7/7.png", "./images/7/8.png", "./images/7/9.png", "./images/7/10.png", "./images/7/11.png", "./images/7/12.png", "./images/7/13.png", "./images/7/14.png", "./images/7/15.png", "./images/7/16.png", "./images/8/1.png", "./images/8/2.png", "./images/8/3.png", "./images/8/4.png", "./images/8/5.png", "./images/8/6.png", "./images/8/7.png", "./images/8/8.png", "./images/8/9.png", "./images/8/10.png", "./images/8/11.png", "./images/8/12.png", "./images/8/13.png", "./images/8/14.png", "./images/8/15.png", "./images/8/16.png", "./images/9/1.png", "./images/9/2.png", "./images/9/3.png", "./images/9/4.png", "./images/9/5.png", "./images/9/6.png", "./images/9/7.png", "./images/9/8.png", "./images/9/9.png", "./images/9/10.png", "./images/9/11.png", "./images/9/12.png", "./images/9/13.png", "./images/9/14.png", "./images/9/15.png", "./images/9/16.png", "./images/grass1.png", "./images/showMore.png"],
       "mousedown": false,
       "animationList": {},
       "layer": null
@@ -176,7 +177,12 @@ window.ozzx.script = {
       var wh = $(window).height();
       var bodySize = this.calculationScene(this);
       this.data.loader = PIXI.loader;
-      this.data.loader.add('./1.gif', {
+      this.data.loader.add("./images/sport.gif", {
+        loadType: PIXI.loaders.Resource.LOAD_TYPE.XHR,
+        xhrType: PIXI.loaders.Resource.XHR_RESPONSE_TYPE.BUFFER,
+        crossOrigin: ''
+      });
+      this.data.loader.add("./images/support.gif", {
         loadType: PIXI.loaders.Resource.LOAD_TYPE.XHR,
         xhrType: PIXI.loaders.Resource.XHR_RESPONSE_TYPE.BUFFER,
         crossOrigin: ''
@@ -216,6 +222,7 @@ window.ozzx.script = {
         _this.openClock(one, oneButterfly, bg1Image);
       });
       this.data.loader.load(function(progress, resources) {
+        _this.data.resources = resources;
         console.log(progress);
         _this.data.animationList.oneButterfly = TweenMax.fromTo(oneButterfly, 0.8, {
           y: _this.data.screenInfo.h * 0.1
@@ -915,38 +922,18 @@ window.ozzx.script = {
           yoyo: true
         });
         cloud7AnimationList.play();
-        var supportH = bodySize.h * 0.8;
-        var support = _this3.methods.createSprite("./images/support.png", {
-          width: supportH * 0.9223,
-          height: supportH,
-          x: bodySize.w * 5 - supportH * 0.9223 / 2 + bodySize.w * 0.5,
-          y: bodySize.h
-        });
-        var supportAnimationList = TweenMax.fromTo(support, 1.5, {
+        var supportH = bodySize.h * 0.8624;
+        var support = new GIF("./images/support.gif", _this3.data.resources);
+        support.sprite.width = supportH * 0.9223;
+        support.sprite.height = supportH;
+        support.sprite.x = bodySize.w * 5 - supportH * 0.9223 / 2 + bodySize.w * 0.5;
+        support.sprite.y = bodySize.h;
+        var supportAnimationList = TweenMax.fromTo(support.sprite, 1.5, {
           y: bodySize.h
         }, {
           y: bodySize.h - supportH
         });
         supportAnimationList.play();
-        var supportTree1H = bodySize.h * 0.2;
-        var supportTree1 = _this3.methods.createSprite("./images/support-tree-1.png", {
-          width: supportTree1H * 0.9223,
-          height: supportTree1H,
-          x: bodySize.w * 5.575 - supportTree1H * 1.1141 / 2,
-          y: bodySize.h
-        });
-        var supportTree1AnimationList = TweenMax.fromTo(supportTree1, 1.5, {
-          y: bodySize.h + supportH * 0.428
-        }, {
-          y: bodySize.h - supportH * 0.572
-        });
-        supportAnimationList.play();
-        var supportTree1AnimationList2 = new TweenMax(supportTree1, 2, {
-          width: supportTree1H * 0.9223 - 10,
-          repeat: -1,
-          yoyo: true
-        });
-        supportTree1AnimationList2.play();
         var showMoreH = bodySize.h * 0.1;
         var showMore = _this3.methods.createSprite("./images/showMore.png", {
           width: showMoreH,
@@ -972,7 +959,7 @@ window.ozzx.script = {
             _this3.domList.showText.style.display = 'block';
           }, 0);
         });
-        _this3.data.twoContainer.addChild(support, supportTree1, showMore);
+        _this3.data.twoContainer.addChild(support.sprite, showMore);
         setTimeout(function() {
           _this3.data.peopleIsMoveing = false;
         }, 200);
@@ -1154,22 +1141,12 @@ window.ozzx.script = {
         }, 200);
       });
       var sportH = bodySize.h * 0.1;
-      var sport = this.methods.createSprite("./images/sport/1.png", {
-        width: sportH * 1.0775,
-        height: sportH,
-        x: bodySize.w * 10 + bodySize.w * 0.88 - sportH * 1.0775 / 2,
-        y: bodySize.h * 0.44 - sportH / 2
-      });
-      var sportInd = 1;
-      setInterval(function() {
-        sport.texture = PIXI.Texture.fromFrame("./images/sport/".concat(sportInd, ".png"));
-        if (sportInd === 1) {
-          sportInd = 2;
-        } else {
-          sportInd = 1;
-        }
-      }, 100);
-      this.data.twoContainer.addChild(bg3Image, this.data.people, bg2Image, tips, threeLight, bg4Image, bg5Image, bg6Image, bg7Image, bg8Image, bg9Image, bg10Image, bg11Image, bgshare, Light11, hand, fiveLight, sevenLight, Light9, shareBT, sport);
+      var sport = new GIF("./images/sport.gif", this.data.resources);
+      sport.sprite.width = sportH * 1.0775;
+      sport.sprite.height = sportH;
+      sport.sprite.x = bodySize.w * 10 + bodySize.w * 0.88 - sportH * 1.0775 / 2;
+      sport.sprite.y = bodySize.h * 0.44 - sportH / 2;
+      this.data.twoContainer.addChild(bg3Image, this.data.people, bg2Image, tips, threeLight, bg4Image, bg5Image, bg6Image, bg7Image, bg8Image, bg9Image, bg10Image, bg11Image, bgshare, Light11, hand, fiveLight, sevenLight, Light9, shareBT, sport.sprite);
       this.data.twoContainer.addChild(cloud3, cloud4, cloud5, cloud6, cloud7, cloud8, cloud9, cloud10, petal11, petal12);
       this.data.app.stage.addChild(this.data.twoContainer);
       var threeLightAnimationList = TweenMax.fromTo(threeLight, 1, {
