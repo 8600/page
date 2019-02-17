b58af4349dd6e0976d52f99f23b9d07be94c9ada
PUGE <100284685@qq.com>
2019-02-17 22:17:16 +0800

    save

--- Foresee you in the future/dist/main.js
+++ Foresee you in the future/dist/main.js
@@ -75,7 +75,6 @@ function pgNameHandler(dom) {
       // 隐藏元素
       tempDom.hide = function () {
         this.style.display = 'none';
-        console.log(this.style);
       };
 
       window.ozzx.domList[pgName.textContent] = tempDom;
@@ -160,7 +159,8 @@ window.onload = function () {
 };
 
 window.ozzx = {
-  script: {}
+  script: {},
+  tool: {}
 };
 var globalConfig = {
   "root": "/src",
@@ -195,7 +195,7 @@ window.ozzx.script = {
       "resources": null,
       "peopleCanMoveing": false,
       "clickPeopleRunning": false,
-      "imgArr": ["./images/1.png", "./images/3-colour.png", "./images/1-clock.png", "./images/1-point.png", "./images/hand.png", "./images/1-hand.png", "./images/1-butterfly.png", "./images/2.png", "./images/3.png", "./images/3-thought.png", "./images/3-start.png", "./images/3-light.png", "./images/5.png", "./images/7.png", "./images/9.png", "./images/11.png", "./images/share.png", "./images/5-colour.png", "./images/7-light.png", "./images/7-colour.png", "./images/9-colour.png", "./images/11-light.png", "./images/11-colour.png", "./images/3-cloud.png", "./images/4-cloud.png", "./images/5-cloud.png", "./images/6-cloud.png", "./images/7-cloud.png", "./images/8-cloud.png", "./images/9-cloud.png", "./images/10-cloud.png", "./images/11-petal-colour.png", "./images/1/1.png", "./images/1/2.png", "./images/1/3.png", "./images/1/4.png", "./images/1/5.png", "./images/1/6.png", "./images/1/7.png", "./images/1/8.png", "./images/1/9.png", "./images/1/10.png", "./images/1/11.png", "./images/1/12.png", "./images/1/13.png", "./images/1/14.png", "./images/1/15.png", "./images/1/16.png", "./images/2/1.png", "./images/2/2.png", "./images/2/3.png", "./images/2/4.png", "./images/2/5.png", "./images/2/6.png", "./images/2/7.png", "./images/2/8.png", "./images/2/9.png", "./images/2/10.png", "./images/2/11.png", "./images/2/12.png", "./images/2/13.png", "./images/2/14.png", "./images/2/15.png", "./images/2/16.png", "./images/3/1.png", "./images/3/2.png", "./images/3/3.png", "./images/3/4.png", "./images/3/5.png", "./images/3/6.png", "./images/3/7.png", "./images/3/8.png", "./images/3/9.png", "./images/3/10.png", "./images/3/11.png", "./images/3/12.png", "./images/3/13.png", "./images/3/14.png", "./images/3/15.png", "./images/3/16.png", "./images/4/1.png", "./images/4/2.png", "./images/4/3.png", "./images/4/4.png", "./images/4/5.png", "./images/4/6.png", "./images/4/7.png", "./images/4/8.png", "./images/4/9.png", "./images/4/10.png", "./images/4/11.png", "./images/4/12.png", "./images/4/13.png", "./images/4/14.png", "./images/4/15.png", "./images/4/16.png", "./images/5/1.png", "./images/5/2.png", "./images/5/3.png", "./images/5/4.png", "./images/5/5.png", "./images/5/6.png", "./images/5/7.png", "./images/5/8.png", "./images/5/9.png", "./images/5/10.png", "./images/5/11.png", "./images/5/12.png", "./images/5/13.png", "./images/5/14.png", "./images/5/15.png", "./images/5/16.png", "./images/6/1.png", "./images/6/2.png", "./images/6/3.png", "./images/6/4.png", "./images/6/5.png", "./images/6/6.png", "./images/6/7.png", "./images/6/8.png", "./images/6/9.png", "./images/6/10.png", "./images/6/11.png", "./images/6/12.png", "./images/6/13.png", "./images/6/14.png", "./images/6/15.png", "./images/6/16.png", "./images/7/1.png", "./images/7/2.png", "./images/7/3.png", "./images/7/4.png", "./images/7/5.png", "./images/7/6.png", "./images/7/7.png", "./images/7/8.png", "./images/7/9.png", "./images/7/10.png", "./images/7/11.png", "./images/7/12.png", "./images/7/13.png", "./images/7/14.png", "./images/7/15.png", "./images/7/16.png", "./images/8/1.png", "./images/8/2.png", "./images/8/3.png", "./images/8/4.png", "./images/8/5.png", "./images/8/6.png", "./images/8/7.png", "./images/8/8.png", "./images/8/9.png", "./images/8/10.png", "./images/8/11.png", "./images/8/12.png", "./images/8/13.png", "./images/8/14.png", "./images/8/15.png", "./images/8/16.png", "./images/9/1.png", "./images/9/2.png", "./images/9/3.png", "./images/9/4.png", "./images/9/5.png", "./images/9/6.png", "./images/9/7.png", "./images/9/8.png", "./images/9/9.png", "./images/9/10.png", "./images/9/11.png", "./images/9/12.png", "./images/9/13.png", "./images/9/14.png", "./images/9/15.png", "./images/9/16.png", "./images/grass1.png", "./images/showMore.png", "./images/showMore2.png", "./images/child.png", "./images/grass2.png", "./images/grass3.png", "./images/3-bubble.png", "./images/3-atom.png", "./images/3-flask.png", "./images/3-star.png", "./images/tree1.png", "./images/tree2.png", "./images/tree3.png", "./images/house1.png", "./images/house2.png", "./images/house3.png", "./images/house4.png", "./images/house5.png", "./images/line-1.png", "./images/line-2.png", "./images/line-3.png", "./images/line-4.png", "./images/share-button.png", "./images/again.png", "./images/11-petal.png", "./images/share-petal-colour.png", "./images/light-hand.png", "./images/5-light.png", "./images/9-light.png", "./images/3-fantasy.png", "./images/9-bird.png"],
+      "imgArr": ["./images/1.jpg", "./images/3-colour.png", "./images/1-clock.png", "./images/1-point.png", "./images/hand.png", "./images/1-hand.png", "./images/1-butterfly.png", "./images/2.png", "./images/3.png", "./images/3-thought.png", "./images/3-start.png", "./images/3-light.png", "./images/5.png", "./images/7.png", "./images/9.png", "./images/11.png", "./images/share.png", "./images/5-colour.png", "./images/7-light.png", "./images/7-colour.png", "./images/9-colour.png", "./images/11-light.png", "./images/11-colour.png", "./images/3-cloud.png", "./images/4-cloud.png", "./images/5-cloud.png", "./images/6-cloud.png", "./images/7-cloud.png", "./images/8-cloud.png", "./images/9-cloud.png", "./images/10-cloud.png", "./images/11-petal-colour.png", "./images/1/1.png", "./images/1/2.png", "./images/1/3.png", "./images/1/4.png", "./images/1/5.png", "./images/1/6.png", "./images/1/7.png", "./images/1/8.png", "./images/1/9.png", "./images/1/10.png", "./images/1/11.png", "./images/1/12.png", "./images/1/13.png", "./images/1/14.png", "./images/1/15.png", "./images/1/16.png", "./images/2/1.png", "./images/2/2.png", "./images/2/3.png", "./images/2/4.png", "./images/2/5.png", "./images/2/6.png", "./images/2/7.png", "./images/2/8.png", "./images/2/9.png", "./images/2/10.png", "./images/2/11.png", "./images/2/12.png", "./images/2/13.png", "./images/2/14.png", "./images/2/15.png", "./images/2/16.png", "./images/3/1.png", "./images/3/2.png", "./images/3/3.png", "./images/3/4.png", "./images/3/5.png", "./images/3/6.png", "./images/3/7.png", "./images/3/8.png", "./images/3/9.png", "./images/3/10.png", "./images/3/11.png", "./images/3/12.png", "./images/3/13.png", "./images/3/14.png", "./images/3/15.png", "./images/3/16.png", "./images/4/1.png", "./images/4/2.png", "./images/4/3.png", "./images/4/4.png", "./images/4/5.png", "./images/4/6.png", "./images/4/7.png", "./images/4/8.png", "./images/4/9.png", "./images/4/10.png", "./images/4/11.png", "./images/4/12.png", "./images/4/13.png", "./images/4/14.png", "./images/4/15.png", "./images/4/16.png", "./images/5/1.png", "./images/5/2.png", "./images/5/3.png", "./images/5/4.png", "./images/5/5.png", "./images/5/6.png", "./images/5/7.png", "./images/5/8.png", "./images/5/9.png", "./images/5/10.png", "./images/5/11.png", "./images/5/12.png", "./images/5/13.png", "./images/5/14.png", "./images/5/15.png", "./images/5/16.png", "./images/6/1.png", "./images/6/2.png", "./images/6/3.png", "./images/6/4.png", "./images/6/5.png", "./images/6/6.png", "./images/6/7.png", "./images/6/8.png", "./images/6/9.png", "./images/6/10.png", "./images/6/11.png", "./images/6/12.png", "./images/6/13.png", "./images/6/14.png", "./images/6/15.png", "./images/6/16.png", "./images/7/1.png", "./images/7/2.png", "./images/7/3.png", "./images/7/4.png", "./images/7/5.png", "./images/7/6.png", "./images/7/7.png", "./images/7/8.png", "./images/7/9.png", "./images/7/10.png", "./images/7/11.png", "./images/7/12.png", "./images/7/13.png", "./images/7/14.png", "./images/7/15.png", "./images/7/16.png", "./images/8/1.png", "./images/8/2.png", "./images/8/3.png", "./images/8/4.png", "./images/8/5.png", "./images/8/6.png", "./images/8/7.png", "./images/8/8.png", "./images/8/9.png", "./images/8/10.png", "./images/8/11.png", "./images/8/12.png", "./images/8/13.png", "./images/8/14.png", "./images/8/15.png", "./images/8/16.png", "./images/9/1.png", "./images/9/2.png", "./images/9/3.png", "./images/9/4.png", "./images/9/5.png", "./images/9/6.png", "./images/9/7.png", "./images/9/8.png", "./images/9/9.png", "./images/9/10.png", "./images/9/11.png", "./images/9/12.png", "./images/9/13.png", "./images/9/14.png", "./images/9/15.png", "./images/9/16.png", "./images/grass1.png", "./images/showMore.png", "./images/showMore2.png", "./images/child.png", "./images/grass2.png", "./images/grass3.png", "./images/3-bubble.png", "./images/3-atom.png", "./images/3-flask.png", "./images/3-star.png", "./images/tree1.png", "./images/tree2.png", "./images/tree3.png", "./images/house1.png", "./images/house2.png", "./images/house3.png", "./images/house4.png", "./images/house5.png", "./images/line-1.png", "./images/line-2.png", "./images/line-3.png", "./images/line-4.png", "./images/share-button.png", "./images/again.png", "./images/11-petal.png", "./images/share-petal-colour.png", "./images/light-hand.png", "./images/5-light.png", "./images/9-light.png", "./images/3-fantasy.png", "./images/9-bird.png"],
       "transverse": true,
       "mousedown": false,
       "animationList": {},
@@ -786,8 +786,7 @@ window.ozzx.script = {
 
         _this3.addBind(showMore, function (e) {
           // console.log(e.data.global)
-          var position = _this3.smartShowBox(bodySize.w, bodySize.h, bodySize.w * 0.4 + 20, 20, e.data.global.x, e.data.global.y);
-
+          var position = ozzx.tool.getPopPosition(bodySize.w, bodySize.h, bodySize.w * 0.4 + 20, 20, e.data.global.x, e.data.global.y);
           setTimeout(function () {
             _this3.domList.showTextBox.style.left = "".concat(position.left, "px");
             _this3.domList.showTextBox.style.top = "".concat(position.top, "px");
@@ -1099,8 +1098,7 @@ window.ozzx.script = {
         showMore.buttonMode = true; // 第三张图 显示更多触摸事件
 
         _this4.addBind(showMore, function (e) {
-          var position = _this4.smartShowBox(bodySize.w, bodySize.h, bodySize.w * 0.4 + 20, 20, e.data.global.x, e.data.global.y);
-
+          var position = ozzx.tool.getPopPosition(bodySize.w, bodySize.h, bodySize.w * 0.4 + 20, 20, e.data.global.x, e.data.global.y);
           setTimeout(function () {
             _this4.domList.showTextBox.style.left = "".concat(position.left, "px");
             _this4.domList.showTextBox.style.top = "".concat(position.top, "px");
@@ -1275,8 +1273,7 @@ window.ozzx.script = {
         showMore.buttonMode = true; // 第三张图 显示更多触摸事件
 
         _this5.addBind(showMore, function (e) {
-          var position = _this5.smartShowBox(bodySize.w, bodySize.h, bodySize.w * 0.4 + 20, 20, e.data.global.x, e.data.global.y);
-
+          var position = ozzx.tool.getPopPosition(bodySize.w, bodySize.h, bodySize.w * 0.4 + 20, 20, e.data.global.x, e.data.global.y);
           setTimeout(function () {
             _this5.domList.showTextBox.style.left = "".concat(position.left, "px");
             _this5.domList.showTextBox.style.top = "".concat(position.top, "px");
@@ -1430,6 +1427,17 @@ window.ozzx.script = {
           // 位置中间靠上
           x: bodySize.w * 7.1,
           y: bodySize.h * 0.22
+        }); // 台子9 比例2.943
+
+
+        var platform9H = bodySize.h * 0.55;
+
+        var platform9 = _this6.methods.createSprite("./images/9-platform.png", {
+          width: platform9H * 2.943,
+          height: platform9H,
+          // 位置中间靠上
+          x: bodySize.w * 7.51 - platform9H * 2.943 / 2,
+          y: bodySize.h - platform9H
         });
 
         var bird9A = new TweenMax(bird9, 4, {
@@ -1458,26 +1466,27 @@ window.ozzx.script = {
         showMore.interactive = true;
         showMore.buttonMode = true; // 显示更多触摸事件
 
-        _this6.addBind(showMore, function () {
+        _this6.addBind(showMore, function (e) {
+          var position = ozzx.tool.getPopPosition(bodySize.w, bodySize.h, bodySize.w * 0.4 + 20, 20, e.data.global.x, e.data.global.y);
           setTimeout(function () {
-            _this6.domList.showTextBox.style.right = "22%";
-            _this6.domList.showTextBox.style.top = "18%";
+            _this6.domList.showTextBox.style.left = "".concat(position.left, "px");
+            _this6.domList.showTextBox.style.top = "".concat(position.top, "px");
             _this6.domList.showTextBox.style.bottom = '';
-            _this6.domList.showTextBox.style.left = '';
+            _this6.domList.showTextBox.style.right = '';
             _this6.domList.textBoxContent.innerText = '一头系着贫，一头系着富，把社会捆绑结实。';
             _this6.domList.showTextBox.style.display = 'block';
           }, 0);
         }); // 群众向上拉 1.22
 
 
-        var massesH = bodySize.h * 0.65;
+        var massesH = bodySize.h * 0.6;
         var masses = new GIF("./images/masses.gif", _this6.data.resources);
         masses.sprite.width = massesH * 1.22;
         masses.sprite.height = massesH;
         masses.sprite.x = bodySize.w * 7.56 - massesH * 1.22 / 2;
-        masses.sprite.y = bodySize.h * 0.555 - massesH / 2;
+        masses.sprite.y = bodySize.h * 0.57 - massesH / 2;
 
-        _this6.data.twoContainer.addChild(bird9, showMore, masses.sprite);
+        _this6.data.twoContainer.addChild(bird9, showMore, platform9, masses.sprite);
 
         setTimeout(function () {
           masses.play();
@@ -1693,7 +1702,7 @@ window.ozzx.script = {
 
         var showMoreH = bodySize.h * 0.1;
 
-        var showMore = _this7.methods.createSprite("./images/showMore.png", {
+        var showMore = _this7.methods.createSprite("./images/showMore2.png", {
           width: showMoreH,
           height: showMoreH,
           x: bodySize.w * 9.77,
@@ -1711,8 +1720,7 @@ window.ozzx.script = {
         showMore.buttonMode = true; // 显示更多触摸事件
 
         _this7.addBind(showMore, function (e) {
-          var position = _this7.smartShowBox(bodySize.w, bodySize.h, bodySize.w * 0.4 + 20, 20, e.data.global.x, e.data.global.y);
-
+          var position = ozzx.tool.getPopPosition(bodySize.w, bodySize.h, bodySize.w * 0.4 + 20, 20, e.data.global.x, e.data.global.y);
           setTimeout(function () {
             _this7.domList.showTextBox.style.top = '';
             _this7.domList.showTextBox.style.right = '';
@@ -2321,28 +2329,40 @@ window.ozzx.script = {
           }
         }
       }
-    },
-    "smartShowBox": function smartShowBox(areaW, areaH, boxW, boxH, x, y) {
-      var left, top; // 先判断横坐标
-      // 距离右侧区域
-
-      if (areaW - x > boxW) {
-        left = x;
-      } else {
-        left = x - boxW;
-      } // 判断纵坐标
-
-
-      if (areaH - y > boxH) {
-        top = y;
-      } else {
-        top = y - boxH;
-      }
-
-      return {
-        left: left,
-        top: top
-      };
     }
   }
+  /**
+  * 智能计算弹窗合适出现的位置
+  * @param  {number} areaW 区域的宽度
+  * @param  {number} areaH 区域的高度
+  * @param  {number} boxW  弹窗的高度
+  * @param  {number} boxH  弹窗的高度
+  * @param  {number} x     出现点的X轴坐标
+  * @param  {number} y     出现点的Y轴坐标
+  * @return {object} 返回合适的位置信息
+  */
+
+};
+
+ozzx.tool.getPopPosition = function (areaW, areaH, boxW, boxH, x, y) {
+  var left, top; // 先判断横坐标
+  // 距离右侧区域
+
+  if (areaW - x > boxW) {
+    left = x;
+  } else {
+    left = x - boxW;
+  } // 判断纵坐标
+
+
+  if (areaH - y > boxH) {
+    top = y;
+  } else {
+    top = y - boxH;
+  }
+
+  return {
+    left: left,
+    top: top
+  };
 };
